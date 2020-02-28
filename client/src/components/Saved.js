e9258d7ac92da8838712d978c02f126a536fc45e
Andy Durette <duretteandy@gmail.com>
2020-02-28 18:28:24 -0500

    Removed unused code

--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,20 +1,6 @@
 import React/*,  {useState, useEffect}*/ from 'react';
 
 function Saved() {
-/*
-  useEffect(() =>{
-    fetchItems();
-  }, []);
-
-  const [items, setItems] = useState([]);
-
-  const fetchItems = async () => {
-    const data = await fetch('https://www.googleapis.com/books/v1/volumes?q=${"Harry Potter"}&key=AIzaSyBCHc9E5LkIumRVSeEdpFUHOUziDAm8Na8');
-    const items = await data.json();
-    console.log(items);
-    setItems(items);
-  }
-  */
 
   return (
     <React.Fragment>
