e828c3239c502d9412fa980061555769b24fcd6f
Andy Durette <duretteandy@gmail.com>
2020-02-28 20:52:38 -0500

    Cleared unused code

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.post('/api/books', async (req, res) => {
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+/*app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+});*/
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
