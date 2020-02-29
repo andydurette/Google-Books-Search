bc101c57d7615211e16bb5e8c41a69f266131b76
Andy Durette <duretteandy@gmail.com>
2020-02-28 20:39:26 -0500

    Added build folder for heroku

---
+++ client/build/precache-manifest.cad684dbc0e976b4eeed6302e726d1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12848e7796ced7917e0a95a99319ecbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5995c419699928a212e",
+    "url": "/static/css/main.3d272db2.chunk.css"
+  },
+  {
+    "revision": "827cff351694cc0dd1e6",
+    "url": "/static/js/2.71b181ff.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.71b181ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5995c419699928a212e",
+    "url": "/static/js/main.0d74e085.chunk.js"
+  },
+  {
+    "revision": "1dd3d1441c85d73e9abb",
+    "url": "/static/js/runtime-main.44e51a38.js"
+  }
+]);
