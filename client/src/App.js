dcf2a61ce13f3102807b27d764266c58ed8a2367
Andy Durette <duretteandy@gmail.com>
2020-03-02 18:56:49 -0500

    Removed Footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import SavedBooks from './components/SavedBooks';
 import MessageModal from './components/MessageModal';
-import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
         </Switch>
         </section>
         <MessageModal/>
-        <Footer/>
       </div>
       </Router>
     </AppProvider> 
