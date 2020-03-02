939c0f3c415560d6fed0de7f641bd1571a387854
Andy Durette <duretteandy@gmail.com>
2020-03-02 16:40:37 -0500

    Updated build folder

---
+++ client/build/precache-manifest.585c44af0e5464b700edbcce461173d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb256deac42228503fa80ce9c9aec20c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03813995eef0590d8ec4",
+    "url": "/static/css/main.04010adf.chunk.css"
+  },
+  {
+    "revision": "87fc31b11f8d3ac30cfc",
+    "url": "/static/js/2.432e3045.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.432e3045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03813995eef0590d8ec4",
+    "url": "/static/js/main.224ba2a3.chunk.js"
+  },
+  {
+    "revision": "1dd3d1441c85d73e9abb",
+    "url": "/static/js/runtime-main.44e51a38.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2b80280f48ef1d8138c1efa011a5ed5.js"
+  "/precache-manifest.585c44af0e5464b700edbcce461173d7.js"
 );
 
 self.addEventListener('message', (event) => {
